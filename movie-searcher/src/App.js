0bd4f9dd2147f83347b530e04ed0571ffc4d14f4
Tamás Mező <tamas.mezo@bixindex.hu>
2020-03-16 20:37:10 +0100

    implementing routes in the app

--- movie-searcher/src/App.js
+++ movie-searcher/src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
-import "./styles/_main.scss";
+import React, { Component } from "react";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import "./App.scss";
 
-function App() {
-  return <div className="App"></div>;
+import SignIn from "./Views/SignIn";
+import SignUp from "./Views/SignUp";
+import Main from "./Views/Main";
+
+class App extends Component {
+  state = {
+    isSignedIn: false
+  };
+
+  render() {
+    let routes = (
+      <Switch>
+        <Route path="/" exact component={SignIn} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/main" exact component={Main} />
+        <Redirect to="/" />
+      </Switch>
+    );
+    if (this.state.isSignedIn) {
+      routes = (
+        <Switch>
+          <Route path="/" exact component={SignIn} />
+          <Route path="/signup" exact component={SignUp} />
+          <Route path="/main" exact component={Main} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+    return <div>{routes}</div>;
+  }
 }
 
-export default App;
+export default withRouter(App);
