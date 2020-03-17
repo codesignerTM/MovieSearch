c63e2d4813875066bfdb3d0647cd872dec4e22ca
Tamás Mező <tamas.mezo@bixindex.hu>
2020-03-17 08:27:52 +0100

    little fix

--- movie-searcher/src/actions/auth.actions.js
+++ movie-searcher/src/actions/auth.actions.js
@@ -3,16 +3,22 @@ import axios from "axios";
 export const Authenticate = (email, password, name, isSignUp) => {
   let url =
     "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAYB2LOT5UckwJR7jf328NizIXlExopAvw";
+  let authData = {
+    email: email,
+    password: password,
+    returnSecureToken: true
+  };
 
   if (isSignUp) {
     url =
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAYB2LOT5UckwJR7jf328NizIXlExopAvw";
+    authData = {
+      email: email,
+      password: password,
+      name: name,
+      returnSecureToken: true
+    };
   }
-  const authData = {
-    email: email,
-    password: password,
-    name: name,
-    returnSecureToken: true
-  };
+
   return axios.post(url, authData);
 };
